416b3cfd5fccc01c82d622705b32bda7c9d2e9b0
sshankars1104 <sshankars.mech@gmail.com>
2024-08-05 14:50:42 +0530

    updated Be URL

--- src/src/pages/Home/Home.jsx
+++ src/src/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     const fetchProperties = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/properties"
+          "https://airbnb-clone-be-l1y1.onrender.com/api/properties"
         );
         setProperties(response.data);
       } catch (err) {
